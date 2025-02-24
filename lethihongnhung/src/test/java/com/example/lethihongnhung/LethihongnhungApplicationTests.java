ec5459060b7d1cdbabf94d6b1990286f03d20ff0
hongnhungle <hongnhung0104@gmail.com>
2025-02-24 10:30:59 +0700

    first commit

---
+++ lethihongnhung/src/test/java/com/example/lethihongnhung/LethihongnhungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lethihongnhung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LethihongnhungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
